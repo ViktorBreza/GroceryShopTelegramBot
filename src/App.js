60a63b1117f68a88322f8e234c476d2fc7e63c48
Viktor Breza <brezaviktor@gmail.com>
2024-09-28 16:41:22 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
-import Header from '../Header/Header';
 import './App.css';
+import Header from './components/Header/Header';
 import { useTelegram } from './useTelegram';
 
 
