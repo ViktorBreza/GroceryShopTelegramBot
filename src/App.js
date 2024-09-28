8c9c46d390100489b1c6e1e03c349c53298d6c54
Viktor Breza <brezaviktor@gmail.com>
2024-09-28 16:56:35 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
-import { useTelegram } from '/hooks/useTelegram';
-
+import { useTelegram } from './hooks/useTelegram';
 
 function App() {
   const {onToggleButton, tg} = useTelegram();
