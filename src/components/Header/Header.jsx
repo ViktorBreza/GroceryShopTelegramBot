989d1cf90ae2e3355794aa12c0925c9f9e29cec8
Viktor Breza <brezaviktor@gmail.com>
2024-10-02 13:35:42 +0300

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,17 +4,19 @@ import Button from '../Button/Button';
 import './Header.css';
 
 const Header = () => {
-    const {user, onClose} = useTelegram();
+    const { user, onClose } = useTelegram();
   
     return (
-        <div className={'header'}>
-            <div className={'welcome-container'}>
-                <span className={'welcome-text'}>
+        <header className="header">
+            <div className="header-content">
+                <span className="welcome-text">
                     Привіт, {user?.username}! Ласкаво просимо до магазину Авокадо, приємних покупок!
                 </span>
+                <Button className="logout-button" onClick={onClose}>
+                    Вийти з магазину
+                </Button>
             </div>
-            <Button className="logout-button" onClick={onClose}>Вийти з магазину</Button>
-        </div>
+        </header>
     );
 };
 
