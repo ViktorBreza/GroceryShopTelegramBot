d725c074670c612ad09b21390cff933a95060ba2
Viktor Breza <brezaviktor@gmail.com>
2024-10-02 10:33:12 +0300

    update

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import './Form.css';
 
-
 const Form = () => {
-    return (
-        <div>
-            Form
-        </div>
+    const [country, setCountry] = useState('');
+    const [street, setStreet] = useState('');
+    const [subject, setSubject] = useState('');
 
+    const onChangeCountry = (e) => {
+        setCountry(e.target.value)
+    }
+
+    const onChangeStreet = (e) => {
+        setStreet(e.target.value)
+    }
+
+    const onChangeSubject = (e) => {
+        setSubject(e.target.value)
+    }
+
+    return (
+        <div className={"form"}>
+            <h3>Введіть ваші дані</h3>
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Країна'}
+                value={country}
+                onChange={onChangeCountry}
+            />
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Вулиця'}
+                value={street}
+                onChange={onChangeStreet}
+            />
+            <select value={subject} onChange={onChangeSubject} className={'select'}>
+                <option value={'physical'}>Фіз. лице</option>
+                <option value={'legal'}>Юр. лице</option>
+            </select>
+        </div>
     );
 };
 
