de7fc30673867a9f2931ab102a8ac627bd8b43b9
Viktor Breza <brezaviktor@gmail.com>
2024-10-03 12:47:41 +0300

    add europ products

--- src/data/products.js
+++ src/data/products.js
@@ -2,8 +2,17 @@
 
 const products = {
    'Товари з Європи': [
-        { id: '1', title: 'Джинси', price: 5000, description: 'Синього кольору, прямі' },
-        { id: '2', title: 'Куртка', price: 12000, description: 'Зеленого кольору, тепла' },
+        { id: '1', title: 'Картопляні чіпси', price: 90, description: 'в асортименті, виробник Польша, 150г', image: '/images/європа/chips.jpg' },
+        { id: '2', title: 'Сік Capri-sun', price: 78, description: 'мультивітамін, 330мл', image: '/images/європа/juice.jpg' },
+        { id: '9', title: 'Кабаноси Berlinki', price: 135, description: 'в асортименті, виробник Польша', image: '/images/європа/kabanosy.jpg' },
+        { id: '10', title: 'Персик в сиропі', price: 150, description: 'половинками, виробник Польша, 400г', image: '/images/європа/persyk.jpg' },
+        { id: '11', title: 'Рисова лапша', price: 65, description: 'безглютенова, виробник Польша, 200г', image: '/images/європа/ryseUdon.jpg' },
+        { id: '12', title: 'Цукерки Skittles', price: 80, description: 'зі смаком фруктів та йогурту, 38г', image: '/images/європа/skittles.jpg' },
+        { id: '13', title: 'Сирний десерт', price: 230, description: 'зі смаком ананасів та миндалю', image: '/images/європа/syrnDes.jpg' },
+        { id: '14', title: 'Чай пакетований Remsey', price: 137, description: 'в асортименті, 20 пакетиків, виробник Польша', image: '/images/європа/tea.jpg' },
+        { id: '15', title: 'М\'ясна консерва Nasze Smaki', price: 120, description: 'виробник Польша, 300г', image: '/images/європа/tushonka.jpg' },
+        { id: '16', title: 'Шоколад Toffifee', price: 110, description: 'білий, 125г', image: '/images/європа/whiteCh.jpg' },
+
     ],
     'Свіжі овочі': [
         { id: '3', title: 'Картопля', price: 6000, description: 'Молода, виробник Узбекистан', image: '/images/овочі/potato.jpg' },
