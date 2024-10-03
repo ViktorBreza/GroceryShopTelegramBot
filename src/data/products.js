5e3f558a1976a457d48063b3ce635e1b7ce12368
Viktor Breza <brezaviktor@gmail.com>
2024-10-03 11:57:55 +0300

    add pictures

--- src/data/products.js
+++ src/data/products.js
@@ -6,14 +6,14 @@ const products = {
         { id: '2', title: 'Куртка', price: 12000, description: 'Зеленого кольору, тепла' },
     ],
     'Свіжі овочі': [
-        { id: '3', title: 'Картопля', price: 6000, description: 'Молода, виробник Узбекистан', image: '/src/data/images/potato.jpg' },
-        { id: '4', title: 'Морква', price: 19, description: 'виробник Єгипет', image: '/src/data/images/carrot.jpg' },
-        { id: '8', title: 'Помідор', price: 7000, description: 'Свіжі, вирощені в Україні', image: '/src/data/images/tomato.jpg' },
+        { id: '3', title: 'Картопля', price: 6000, description: 'Молода, виробник Узбекистан', image: '/images/potato.jpg' },
+        { id: '4', title: 'Морква', price: 19, description: 'виробник Єгипет', image: '/images/carrot.jpg' },
+        { id: '8', title: 'Помідор', price: 7000, description: 'Свіжі, вирощені в Україні', image: '/images/tomato.jpg' },
     ],
     'Свіжі фрукти': [
-        { id: '5', title: 'Яблуко', price: 76, description: 'солодке, виробник Україна', image: '/src/data/images/apple.jpg' },
-        { id: '6', title: 'Авокадо', price: 5000, description: 'Стиглі, з Мексики', image: '/src/data/images/avocado.jpg' },
-        { id: '7', title: 'Банан', price: 128, description: 'великий, виробник Японія', image: '/src/data/images/banana.jpg' },
+        { id: '5', title: 'Яблуко', price: 76, description: 'солодке, виробник Україна', image: '/images/apple.jpg' },
+        { id: '6', title: 'Авокадо', price: 5000, description: 'Стиглі, з Мексики', image: '/images/avocado.jpg' },
+        { id: '7', title: 'Банан', price: 128, description: 'великий, виробник Японія', image: '/images/banana.jpg' },
     ],
 };
 
